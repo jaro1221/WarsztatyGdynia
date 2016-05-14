6b4f45126d6dc9049a195840ff437fa430df5194
Jarek Przyjemski <jaro1221@windowslive.com>
2016-05-14 13:13:14 +0200

    Add project files.

---
+++ Warsztaty1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Warsztaty1.Models;
+
+namespace Warsztaty1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            db.Users.FirstOrDefault(x => x.Id == "");
+            return View();
+        }
+
+        [Authorize]
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Warsztaty1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Warsztaty1.MvcApplication" Language="C#" %>
